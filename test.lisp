f01f4a16d82bf557733327b2ee953849d95c48d2
Helmut Kian <helmutkian@Helmut-X1.gateway.pace.com>
2016-01-03 05:07:32 -0800

    Separate tests for sync & async callbacks to CL from JS. Test return values from callbacks to CL from JS. Update CL eval from JS test.

--- test.lisp
+++ test.lisp
@@ -10,19 +10,41 @@
     (5am:is (= 1 (js:call-function js-fn)))
     (js:free js-fn)))
 
-(5am:test test-async-return-to-cl
+(5am:test test-cl-callback
   (let ((js-fn (js:evaluate "(function (cb) { cb('test'); });"))
 	(result nil))
     (js:call-function js-fn
-		      (js:make-callback (lambda (x) (setf result x))))
-    (js:tick)
+		      (js:make-callback
+		       (lambda (x)
+			 (setf result x)
+			 ;; Return null
+			 (values))))
     (5am:is (string= "test" result))
     (js:free js-fn)))
 
+
+(5am:test test-cl-callback-async
+  (let ((js-fn (js:evaluate "(function (cb) { setTimeout(function () { cb('foo'); }, 0); })"))
+	(result nil))
+    (js:call-function js-fn
+		      (js:make-callback
+		       (lambda (x)
+			 (setf result x)
+			 ;; Return null
+			 (values))))
+    (5am:is (null result))
+    (loop until (zerop (js:tick)))
+    (5am:is (string= "foo" result))))
+
+
+(5am:test test-return-from-cl-callback-to-js
+  (let ((js-fn (js:evaluate "(function (f) { return f(); })")))
+    (5am:is (= 111
+	       (js:call-function js-fn (js:make-callback (lambda () 111)))))))
+  
 (5am:test test-cl-evaluate
   (5am:is
-   (string= "foobar"
-	    (js:evaluate "('foo' + CL.evaluate('(concatenate \\'string \"ba\" \"r\")'))"))))
+   (= 7 (js:evaluate "(1 + process.natives.cl_eval('(+ 1 2 3)'))"))))
 
 (defun run-tests ()
   (5am:run! 'cl-node.test))
